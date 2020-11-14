65049103b2fed97d3e4151029b6c49a67865391f
furkan-cloud <firstinfirstout2020@gmail.com>
2020-11-14 22:03:36 +0300

    add searchbutton click2

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import './App.css';
 import {SearchBox} from "./components/SearchBox";
 import {CardList} from "./components/CardList";
 
-const apiKey = ""; //temporary
+const apiKey = "7fc8d00ab1302c6aa7da237d2810116e"; //temporary
 const baseUrl = "https://api.themoviedb.org/3/search/movie";
 const baseImageUrl = "https://image.tmdb.org/t/p/w500";
 
@@ -13,23 +13,24 @@ const baseImageUrl = "https://image.tmdb.org/t/p/w500";
 function App() {
 
   const [movieList, setMovieList] = useState([]);
+  const [searchKeyword, setSearchKeyword] = useState("Matrix")
 
   useEffect(()=>{
       axios.get(
           baseUrl, {params: {
           api_key: apiKey,
           page: 1,
-          query: "Star Wars"  //TODO: from input
+          query: searchKeyword
           }}
       )
       .then((res)=> setMovieList(res?.data?.results))
       .catch()
       .finally()
-  },[])
+  },[searchKeyword])
 
   return (
     <div className="App">
-      <SearchBox/>
+      <SearchBox setSearchKeyword={setSearchKeyword}/>
       <CardList movieList={movieList} baseImageUrl={baseImageUrl} />
     </div>
   );
